c125b537341e37589ed098734afafb782e341809
taufiq <taufiq6040@gmail.com>
2026-06-23 09:05:29 +0800

    var

---
+++ var.js
@@ -0,0 +1,9 @@
+var nama = "taufiq";
+var nomor_urut = 11;
+
+console.log(nama);
+console.log(nomor_urut);
+
+var buka_pintu = true;
+if (buka_pintu == true)
+console.log(buka_pintu);
--- variabel.js
+++
@@ -1,8 +0,0 @@
-var nama = "taufiq";
-var nomor_urut = 11;
-console.log(nama);
-console.log(nomor_urut);
-console.log("nama");
-console.log("nomor_urut");
-console.log(nomor_urut);
-console.log(nomor_urut)
