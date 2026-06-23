7ea5516c779d4585f467483834e5b26a4e812c77
taufiq <taufiq6040@gmail.com>
2026-06-23 08:51:00 +0800

    var

--- variabel.js
+++ variabel.js
@@ -2,3 +2,7 @@ var nama = "taufiq";
 var nomor_urut = 11;
 console.log(nama);
 console.log(nomor_urut);
+console.log("nama");
+console.log("nomor_urut");
+console.log(nomor_urut);
+console.log(nomor_urut)
