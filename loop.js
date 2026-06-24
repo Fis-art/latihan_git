45b8c87da328d1d494248514e6e63f48537f5d93
taufiq <taufiq6040@gmail.com>
2026-06-24 11:28:41 +0800

    loop

--- loop.js
+++ loop.js
@@ -1,4 +1,16 @@
 
-for (var angka = -100; angka < 10; angka++) { // isi angka dengan nilai awal dan batas akhir yang diinginkan
-    console.log("iterasi ke : " + angka);
+// for (var angka = 1; angka <= 10; angka++) { // isi angka dengan nilai awal dan batas akhir yang diinginkan
+//     console.log("iterasi ke : " + angka);
+// }
+
+var jumlah = 0;
+for (var deret = 5; deret > 0; deret--) {
+    jumlah += deret;
 }
+var jumlah = 0;
+for (var deret = 5; deret > 0; deret--) {
+    jumlah += deret;
+    console.log("Jumlah saat ini: " + jumlah);
+}
+
+console.log("jumlah akhir: " + jumlah);
