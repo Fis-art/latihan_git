cf10547faf82b1662088e0fbb11ee555817a5f3f
taufiq <taufiq6040@gmail.com>
2026-06-24 11:05:38 +0800

    cond

--- cond.js
+++ cond.js
@@ -1,31 +1,92 @@
-var mood = "sedih";
-if (mood == "senang") {
-    console.log("Saya senang hari ini!");
-} else {
-    console.log("Saya tidak senang hari ini.");
-}
-if (false) {
-    console.log("code tidak dijalankan");
-}
+// // if  (true) {
+// //     console.log("jalankan code ini ");
+// // }
+// // if  (false) {
+// //     console.log("code tidak dijalankan");
+// // }
 
-var mood = "happy";
-if (mood == "happy") {
-    console.log("Saya senang hari ini!");
-}   else {
-    console.log("Saya tidak senang hari ini.");
-}
+// // var mood = "happy";
+// // if (mood == "happy") {
+// //     console.log("Saya senang hari ini!");
+// // }   else {
+// //     console.log("Saya tidak senang hari ini.");
+// // }
+// // var angka = 1;
+// // if (angka == "1") {
+// //     console.log("angka 1");
+// // } else {
+// //     console.log("tidak muncul");
+// // }
 
-var umur = 16;
-if (umur >= 17) {
-    console.log("Boleh masuk");
-} else {
-    console.log("Masih Dibawah Umur Belum boleh masuk");
-}
+// // var mood = "happy";
+// // if (mood == "happy") {
+// //     console.log("Saya senang hari ini!");
+// // } else {
+// //     console.log("Saya tidak senang hari ini.");
+// // }
 
+// // var mood = "grumpy";
+// // if (mood == "happy") {
+// //     console.log("Saya senang hari ini!");
+// // } else if (mood == "sad") {
+// //     console.log("Saya tidak senang hari ini.");
+// // } else  {
+// //     console.log("saya tidak baik-baik saja");
+// // }
 
-if  (true) {
-    console.log("jalankan code ini ");
-}
-if  (false) {
-    console.log("code tidak dijalankan");
-}
+// // var umur = -10;
+// // if (umur <= 17) {
+// //     console.log("anda masih di bawah umur");
+// // } else {
+// //     console.log("anda sudah dewasa");
+// // }
+
+// // var umur = 35 ; // isi dengan umur yang ingin diuji
+// // if(umur >= 17){
+// //     console.log("sudah memiliki KTP")
+// //     if(umur > 20){
+// //         console.log("umur kurang dari 20 tahun")
+// //     } else if(umur > 30){
+// //         console.log("usia diantara 20-30 tahun")
+// //     } else if (umur > 40){
+// //         console.log("usia diantara 30-40 tahun")
+// //     } else{
+// //         console.log("sudah berumur")
+// //     }
+// // } else {
+// //     console.log("belum memiliki KTP")
+// // }
+
+// // var umur = 35; // isi dengan umur yang ingin diuji
+// // if (umur >= 17) {
+// //     console.log("anda memiliki ktp");
+// //     if (umur > 20) {
+// //         console.log("umur kurang dari 20");
+// //     }
+// //     else if (umur > 30) {
+// //         console.log("umur antara 20-30");
+// //     }
+// //     else if (umur > 40) {
+// //         console.log("umur antara 30-40");
+// //     }
+// //     else {
+// //         console.log("anda sudah berumur");
+// //     }
+// // } else {
+// //     console.log("anda tidak punya ktp");
+// // }
+
+// var warna = "kuning"; // isi dengan warna yang ingin diuji
+// switch (warna) {
+//     case "merah":
+//         console.log("Warna merah");
+//         break;
+//     case "biru":
+//         console.log("Warna biru");
+//         break;
+//     case "hijau":
+//         console.log("Warna hijau");
+//         break;
+//     default:
+//         console.log("Warna tidak dikenal");
+// }
---
+++ loop.js
@@ -0,0 +1,4 @@
+
+for (var angka = 1; angka < 10; angka++) {
+    console.log("angka: " + angka);
+}
