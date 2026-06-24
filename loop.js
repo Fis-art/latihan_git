dfb761b11df1baf39adc2a13fb4046fb17743d97
taufiq <taufiq6040@gmail.com>
2026-06-24 11:09:28 +0800

    loop

--- loop.js
+++ loop.js
@@ -1,4 +1,4 @@
 
-for (var angka = 1; angka < 10; angka++) {
-    console.log("angka: " + angka);
+for (var angka = -100; angka < 10; angka++) { // isi angka dengan nilai awal dan batas akhir yang diinginkan
+    console.log("iterasi ke : " + angka);
 }
