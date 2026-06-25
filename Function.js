2d64fe81954156ac0deca15d0793504af8d219a5
taufiq <taufiq6040@gmail.com>
2026-06-25 09:43:40 +0800

    function

---
+++ Function.js
@@ -0,0 +1,49 @@
+//Function Javascript
+//Function adalah sebuah blok kode yang disusun sedemikian rupa untuk menjalankan sebuah tindakan. 
+//Blok kode ini dibuat untuk dapat bisa digunakan kembali.
+
+//contoh 1
+function Tampilkan(){
+    console.log("halo!");
+}
+Tampilkan();
+
+//latihan 1
+var nama = "saya taufik"
+function tampilkan(){
+    console.log("halo!",nama);
+ 
+}
+tampilkan()
+
+
+// //contoh 2
+// function MunculkanAngkaDua(){
+//     return 2
+// }
+// // var tampung = MunculkanAngkaDua();
+// // console.log(tampung)
+// console.log(MunculkanAngkaDua())
+
+
+// //contoh 3
+// function kalikanDua(angka) {
+//     return angka * 2
+// }
+// var tampung = kalikanDua(2);
+// console.log(tampung)
+// // console.log(kalikanDua())
+
+// //contoh 4
+// function TampilkanAngka(AngkaPertama,AngkaKedua=4){
+//     return AngkaPertama + AngkaKedua
+// }
+
+// console.log(TampilkanAngka(5,3))
+// console.log(TampilkanAngka(6))
+
+
+
+
+//Anonymous Function
+//contoh 1
--- array.js
+++ array.js
@@ -1,68 +1,86 @@
-var angka = [9,7,5,3,1]
-console.log(angka)
-console.log(angka[1])
-console.log(angka[4])
-
-// array method
-//length
-console.log(angka.length)
-//push
-console.log("=========","sebelum push :" + angka)
-angka.push(6)
-console.log("=========","sesudah push :" + angka)
-//pop
-console.log("=========","sebelum pop : "+ angka)
-angka.pop()
-console.log("=========","sesudah pop :" + angka)
-//unshift
-console.log("=========","sebelum unshift :" + angka)
-angka.unshift(8)
-console.log("=========","sesudah unshift :" + angka)
-//shift
-console.log("=========","sebelum shift :" + angka)
-angka.shift()
-console.log("=========","sesudah shift :" + angka)
+// //Loop/Iteration adalah tindakan mengulang / merepetisi sebuah proses
 
 
-//join
-var peserta = ["Andra","Taufiq","Adit","Debby"]
-console.log(peserta)
-console.log("=========","sebelum join")
-console.log(peserta)
-console.log("=========","sesudah join")
-console.log(peserta.join("-"))
+// var angka = [9,7,5,3,1]
+// console.log(angka)
+// console.log(angka[1])
+// console.log(angka[4])
 
-//split
-var nama = "Muhammad Desta Greddy Aulia Rahman"
-console.log("=========","sebelum Split")
-console.log(nama)
-console.log("=========","setelah Split")
-console.log(nama.split(" ").length)
+// // array method
+// //length
+// console.log(angka.length)
+// //push
+// console.log("=========","sebelum push :" + angka)
+// angka.push(6)
+// console.log("=========","sesudah push :" + angka)
+// //pop
+// console.log("=========","sebelum pop : "+ angka)
+// angka.pop()
+// console.log("=========","sesudah pop :" + angka)
+// //unshift
+// console.log("=========","sebelum unshift :" + angka)
+// angka.unshift(8)
+// console.log("=========","sesudah unshift :" + angka)
+// //shift
+// console.log("=========","sebelum shift :" + angka)
+// angka.shift()
+// console.log("=========","sesudah shift :" + angka)
 
-//sort
-var peserta = ["Andra","Taufiq","Adit","Debby"]
-console.log("=========","sebelum Sort")
-console.log(peserta)
-console.log("=========","Sesudah Sort")
-console.log(peserta.sort())
-console.log("=========","sebelum Reverse ")
-console.log(peserta.reverse())
 
-//slice
+// //join
+// var peserta = ["Andra","Taufiq","Adit","Debby"]
+// console.log(peserta)
+// console.log("=========","sebelum join")
+// console.log(peserta)
+// console.log("=========","sesudah join")
+// console.log(peserta.join("-"))
 
-var nama = ["Andra","Taufiq","Adit","Debby"]
-console.log("=========","sebelum slice")
-console.log(nama)
-console.log("=========","Sesudah Slice")
-var newpeserta = peserta.slice(0,2)
-console.log(newpeserta)
-console.log(peserta)
+// //split
+// var nama = "Muhammad Desta Greddy Aulia Rahman"
+// console.log("=========","sebelum Split")
+// console.log(nama)
+// console.log("=========","setelah Split")
+// console.log(nama.split(" ").length)
 
-//Splice
+// //sort
+// var peserta = ["Andra","Taufiq","Adit","Debby"]
+// console.log("=========","sebelum Sort")
+// console.log(peserta)
+// console.log("=========","Sesudah Sort")
+// console.log(peserta.sort())
+// console.log("=========","sebelum Reverse ")
+// console.log(peserta.reverse())
 
-var nama = ["Andra","Taufiq","Adit","Debby"]
-console.log("=========","sebelum splice")
-console.log(nama)
-console.log("=========","Sesudah splice")
-peserta.splice(1,2,"Heldi","Huda")
-console.log(peserta)
+// //slice
+
+// var nama = ["Andra","Taufiq","Adit","Debby"]
+// console.log("=========","sebelum slice")
+// console.log(nama)
+// console.log("=========","Sesudah Slice")
+// var newpeserta = peserta.slice(0,2)
+// console.log(newpeserta)
+// console.log(peserta)
+
+// //Splice
+
+// var nama = ["Andra","Taufiq","Adit","Debby"]
+// console.log("=========","sebelum splice")
+// console.log(nama)
+// console.log("=========","Sesudah splice")
+// peserta.splice(1,2,"Heldi","Huda")
+// console.log(peserta)
+
+// Looping Array
+
+//contoh 1
+var arr = ['jeruk','mangga','apel'];
+for (var buah= 0; buah < arr.length;buah++){
+    console.log(arr[buah])
+}
+// contoh 2
+var nama = ["Heldi","Huda","Rendra","Desta"]
+var i = 0
+while (i < nama.length){
+    console.log(nama[i])
+    i++
+}
