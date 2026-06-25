68059a1d7019d46821ccef0d98d169dbec4c8967
taufiq <taufiq6040@gmail.com>
2026-06-25 10:47:09 +0800

    function

--- Function.js
+++ Function.js
@@ -1,49 +1,57 @@
-//Function Javascript
-//Function adalah sebuah blok kode yang disusun sedemikian rupa untuk menjalankan sebuah tindakan. 
-//Blok kode ini dibuat untuk dapat bisa digunakan kembali.
+// //Function Javascript
+// //Function adalah sebuah blok kode yang disusun sedemikian rupa untuk menjalankan sebuah tindakan. 
+// //Blok kode ini dibuat untuk dapat bisa digunakan kembali.
 
-//contoh 1
-function Tampilkan(){
-    console.log("halo!");
-}
-Tampilkan();
+// // //contoh 1
+// // function Tampilkan(){
+// //     console.log("halo!");
+// // }
+// // Tampilkan();
 
-//latihan 1
-var nama = "saya taufik"
-function tampilkan(){
-    console.log("halo!",nama);
+// //latihan 1
+// // var nama = "saya taufik"
+// // var kena = "qurrahman"
+// // function tampilkan(){
+// //     console.log("halo!",nama);
  
-}
-tampilkan()
+// // }
+// // tampilkan()
 
 
-// //contoh 2
-// function MunculkanAngkaDua(){
-//     return 2
-// }
-// // var tampung = MunculkanAngkaDua();
+// // //contoh 2
+// // function MunculkanAngkaDua(){
+// //     return 2
+// // }
+// // // var tampung = MunculkanAngkaDua();
+// // // console.log(tampung)
+// // console.log(MunculkanAngkaDua())
+
+
+// // //contoh 3
+// // function kalikanDua(angka) {
+// //     return angka * 2
+// // }
+// // var tampung = kalikanDua(2);
 // // console.log(tampung)
-// console.log(MunculkanAngkaDua())
+// // // console.log(kalikanDua())
+
+// // //contoh 4
+// // function TampilkanAngka(AngkaPertama,AngkaKedua=4){
+// //     return AngkaPertama + AngkaKedua
+// // }
+
+// // console.log(TampilkanAngka(5,3))
+// // console.log(TampilkanAngka(6))
 
 
-// //contoh 3
-// function kalikanDua(angka) {
-//     return angka * 2
+// //Anonymous Function
+// //Kita juga dapat menampung function sebagai variable dengan sebuah bentuk function yang dinamakan Anonymous Function.
+
+// //contoh 1
+
+// var HitungAngka = function(a, b) {
+//     return a * b
 // }
-// var tampung = kalikanDua(2);
-// console.log(tampung)
-// // console.log(kalikanDua())
-
-// //contoh 4
-// function TampilkanAngka(AngkaPertama,AngkaKedua=4){
-//     return AngkaPertama + AngkaKedua
-// }
-
-// console.log(TampilkanAngka(5,3))
-// console.log(TampilkanAngka(6))
+// console.log(HitungAngka(2,5,7))
 
 
-
-
-//Anonymous Function
-//contoh 1
---
+++ Object.js
@@ -0,0 +1,51 @@
+//Object Javascript
+//Object adalah kumpulan data tidak berurut yang berisikan pasangan property (key) dan value . 
+// Jika kita ingat pada tipe data Array yang merupakan kumpulan data yang berurut sesuai indeks, 
+//sedangkan Object mirip dengan Array tapi kini kita memberikan property atau key sendiri.
+
+//cara menuliskan Object menggunakan kurung kurawal ({}) berbeda dengan array yang menggunakan kurung siku.
+
+//Deklarasi Object
+//contoh
+
+// contoh object
+
+var orang = [
+     {
+    nama : "taufik",
+    warnaKulit : "kuning langsat",
+    jenisKelamin : "Laki-Laki",
+    tinggiBadan : 160,
+    beratBadan : 55,
+    usia : 24,
+     }
+]
+console.log(orang[0])
+
+
+
+
+
+var identitas = [
+     {
+    nama : "Adit",
+    warnaKulit : "kuning langsat",
+    jenisKelamin : "Laki-Laki",
+    tinggiBadan : 168,
+    beratBadan : 85,
+    usia : 21,
+},
+{
+    nama : "Andra",
+    warnaKulit : "kuning langsat",
+    tinggiBadan : 170,
+    beratBadan : 60,
+    usia : 18,
+}
+]
+console.log(identitas[0])
+console.log(identitas[1])
+
+
+
+
