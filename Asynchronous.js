af773b6ed93d49b5a1c2fc36ef05df27441b7d18
taufiq <taufiq6040@gmail.com>
2026-06-26 14:48:59 +0800

    Async

--- Asynchronous.js
+++ Asynchronous.js
@@ -47,46 +47,54 @@
 // }
 // askMom ()
 
-// contoh promise lainnya (promise memiliki parameter)
-function periksaDataPasien(nomorIdPasien){ //penulisan function bisa
-    var dataPasien = [
-        {id : 1, nama: "John", jenisKelamin : "Laki-Laki"},
-        {id : 2, nama: "Michael", jenisKelamin : "Laki-Laki"},
-        {id : 3, nama: "Sarah", jenisKelamin : "Perempuan"},
-        {id : 4, nama: "Frank", jenisKelamin : "Laki-Laki"},
-    ]
-    return new Promise( function (resolve, reject){
-        var pasien = dataPasien.find(x=> x.id === nomorIdPasien)
-        if (pasien === undefined){
-            reject("data pasien tidak ada")
-        }else {
-            resolve(pasien)
-        }
-    })
-}
-//Cara menggunakan Promise yang memiliki parameter
-periksaDataPasien(5).then(function(data){  //input no id
-    console.log(data)
-}).catch(function(err){
-    console.log(err)
-})
-
-
-//Apa itu Async/Await?
-//Async/await adalah fitur yang hadir sejak ES2017. Fitur ini mempermudah kita dalam menangani proses asynchronous.
-
-// function doAsync(){
-//     return new Promise ( function (resolve, reject){
-//         var check = true
-//         if (check) {
-//             resolve("berhasil")
-//         } else {
-//             reject("gagal")
+// // contoh promise lainnya (promise memiliki parameter)
+// function periksaDataPasien(nomorIdPasien){ //penulisan function bisa
+//     var dataPasien = [
+//         {id : 1, nama: "John", jenisKelamin : "Laki-Laki"},
+//         {id : 2, nama: "Michael", jenisKelamin : "Laki-Laki"},
+//         {id : 3, nama: "Sarah", jenisKelamin : "Perempuan"},
+//         {id : 4, nama: "Frank", jenisKelamin : "Laki-Laki"},
+//     ]
+//     return new Promise( function (resolve, reject){
+//         // var pasien = dataPasien.find(x=> x.id === nomorIdPasien)         // arrow function 
+//         // var pasien = dataPasien.find(function(x)                            // function Declaration / function biasa
+//         // {return x.id === nomorIdPasien})
+        
+//         if (pasien === undefined){
+//             reject("data pasien tidak ada")
+//         }else {
+//             resolve(pasien)
 //         }
 //     })
 // }
-// async function hello () {
-//     var result = await doAsync()
-//     console.log(result)
-// }
-// hello()
+// //Cara menggunakan Promise yang memiliki parameter
+// periksaDataPasien(1)        //input no id
+// .then(function(data){  
+//     console.log(data)}
+// ).catch(function(err){
+//     console.log(err)})
+
+
+//Apa itu Async/Await
+//Async/await adalah fitur yang hadir sejak ES2017. Fitur ini mempermudah kita dalam menangani proses asynchronous.
+
+function doAsync(){
+    return new Promise ( function (resolve, reject){
+        var check = false
+        if (check) {
+            resolve("berhasil")
+        } else {
+            var gagal = new Error("anda kurang beruntung");
+            reject(gagal)
+        }
+    })
+}
+async function hello () {
+    try{
+    var result = await doAsync()
+    console.log(result)
+    } catch(err){
+        console.log(err.message)
+    }
+}
+hello()
