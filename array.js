5355e09af29e1a87824c5d5e01b003bef30894ce
taufiq <taufiq6040@gmail.com>
2026-06-26 09:55:53 +0800

    array finish

--- array.js
+++ array.js
@@ -1,86 +1,86 @@
-// //Loop/Iteration adalah tindakan mengulang / merepetisi sebuah proses
+// // //Loop/Iteration adalah tindakan mengulang / merepetisi sebuah proses
 
 
-// var angka = [9,7,5,3,1]
-// console.log(angka)
-// console.log(angka[1])
-// console.log(angka[4])
+// // var angka = [9,7,5,3,1]
+// // console.log(angka)
+// // console.log(angka[1])
+// // console.log(angka[4])
 
-// // array method
-// //length
-// console.log(angka.length)
-// //push
-// console.log("=========","sebelum push :" + angka)
-// angka.push(6)
-// console.log("=========","sesudah push :" + angka)
-// //pop
-// console.log("=========","sebelum pop : "+ angka)
-// angka.pop()
-// console.log("=========","sesudah pop :" + angka)
-// //unshift
-// console.log("=========","sebelum unshift :" + angka)
-// angka.unshift(8)
-// console.log("=========","sesudah unshift :" + angka)
-// //shift
-// console.log("=========","sebelum shift :" + angka)
-// angka.shift()
-// console.log("=========","sesudah shift :" + angka)
+// // // array method
+// // //length
+// // console.log(angka.length)
+// // //push
+// // console.log("=========","sebelum push :" + angka)
+// // angka.push(6)
+// // console.log("=========","sesudah push :" + angka)
+// // //pop
+// // console.log("=========","sebelum pop : "+ angka)
+// // angka.pop()
+// // console.log("=========","sesudah pop :" + angka)
+// // //unshift
+// // console.log("=========","sebelum unshift :" + angka)
+// // angka.unshift(8)
+// // console.log("=========","sesudah unshift :" + angka)
+// // //shift
+// // console.log("=========","sebelum shift :" + angka)
+// // angka.shift()
+// // console.log("=========","sesudah shift :" + angka)
 
 
-// //join
-// var peserta = ["Andra","Taufiq","Adit","Debby"]
-// console.log(peserta)
-// console.log("=========","sebelum join")
-// console.log(peserta)
-// console.log("=========","sesudah join")
-// console.log(peserta.join("-"))
+// // //join
+// // var peserta = ["Andra","Taufiq","Adit","Debby"]
+// // console.log(peserta)
+// // console.log("=========","sebelum join")
+// // console.log(peserta)
+// // console.log("=========","sesudah join")
+// // console.log(peserta.join("-"))
 
-// //split
-// var nama = "Muhammad Desta Greddy Aulia Rahman"
-// console.log("=========","sebelum Split")
-// console.log(nama)
-// console.log("=========","setelah Split")
-// console.log(nama.split(" ").length)
+// // //split
+// // var nama = "Muhammad Desta Greddy Aulia Rahman"
+// // console.log("=========","sebelum Split")
+// // console.log(nama)
+// // console.log("=========","setelah Split")
+// // console.log(nama.split(" ").length)
 
-// //sort
-// var peserta = ["Andra","Taufiq","Adit","Debby"]
-// console.log("=========","sebelum Sort")
-// console.log(peserta)
-// console.log("=========","Sesudah Sort")
-// console.log(peserta.sort())
-// console.log("=========","sebelum Reverse ")
-// console.log(peserta.reverse())
+// // //sort
+// // var peserta = ["Andra","Taufiq","Adit","Debby"]
+// // console.log("=========","sebelum Sort")
+// // console.log(peserta)
+// // console.log("=========","Sesudah Sort")
+// // console.log(peserta.sort())
+// // console.log("=========","sebelum Reverse ")
+// // console.log(peserta.reverse())
 
-// //slice
+// // //slice
 
-// var nama = ["Andra","Taufiq","Adit","Debby"]
-// console.log("=========","sebelum slice")
-// console.log(nama)
-// console.log("=========","Sesudah Slice")
-// var newpeserta = peserta.slice(0,2)
-// console.log(newpeserta)
-// console.log(peserta)
+// // var nama = ["Andra","Taufiq","Adit","Debby"]
+// // console.log("=========","sebelum slice")
+// // console.log(nama)
+// // console.log("=========","Sesudah Slice")
+// // var newpeserta = peserta.slice(0,2)
+// // console.log(newpeserta)
+// // console.log(peserta)
 
-// //Splice
+// // //Splice
 
-// var nama = ["Andra","Taufiq","Adit","Debby"]
-// console.log("=========","sebelum splice")
-// console.log(nama)
-// console.log("=========","Sesudah splice")
-// peserta.splice(1,2,"Heldi","Huda")
-// console.log(peserta)
+// // var nama = ["Andra","Taufiq","Adit","Debby"]
+// // console.log("=========","sebelum splice")
+// // console.log(nama)
+// // console.log("=========","Sesudah splice")
+// // peserta.splice(1,2,"Heldi","Huda")
+// // console.log(peserta)
 
-// Looping Array
+// // Looping Array
 
-//contoh 1
-var arr = ['jeruk','mangga','apel'];
-for (var buah= 0; buah < arr.length;buah++){
-    console.log(arr[buah])
-}
-// contoh 2
-var nama = ["Heldi","Huda","Rendra","Desta"]
-var i = 0
-while (i < nama.length){
-    console.log(nama[i])
-    i++
-}
+// //contoh 1
+// var arr = ['jeruk','mangga','apel'];
+// for (var buah= 0; buah < arr.length;buah++){
+//     console.log(arr[buah])
+// }
+// // contoh 2
+// var nama = ["Heldi","Huda","Rendra","Desta"]
+// var i = 0
+// while (i < nama.length){
+//     console.log(nama[i])
+//     i++
+// }
