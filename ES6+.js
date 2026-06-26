14956d0bf9b7997d69cf439bee40a57f95e188f6
taufiq <taufiq6040@gmail.com>
2026-06-26 09:44:02 +0800

    es6+

--- ES6+.js
+++ ES6+.js
@@ -1,103 +1,170 @@
-// //Apa itu ES6
+// // //Apa itu ES6
 
-// //ES6 atau EcmaScript 6 atau EcmaScript 2015 merupakan fitur bahasa pada pemrograman Javascript modern atau biasa di sebut next gen javascript. 
-// //lalu kenapa kita harus mempelajari ES6, itu karena library maupun framework JS terkini banyak menggunakan sintaks-sintaks ES6.
-// //setelah EcmaScript 6 ini banyak fitur-fitur yang dapat digunakan untuk mengoptimalkan
+// // //ES6 atau EcmaScript 6 atau EcmaScript 2015 merupakan fitur bahasa pada pemrograman Javascript modern atau biasa di sebut next gen javascript. 
+// // //lalu kenapa kita harus mempelajari ES6, itu karena library maupun framework JS terkini banyak menggunakan sintaks-sintaks ES6.
+// // //setelah EcmaScript 6 ini banyak fitur-fitur yang dapat digunakan untuk mengoptimalkan
 
-// // let = 
+// // // let = 
 
-// let nama = "John"
-//     if(true){
-//     let nama = "Doe"
-//     console.log(nama)
-//     }
-// console.log(nama)
+// // let nama = "John"
+// //     if(true){
+// //     let nama = "Doe"
+// //     console.log(nama)
+// //     }
+// // console.log(nama)
 
-// const pi = 3.14
+// // const pi = 3.14
 
-// //Arrow Function
-// const myFunc =()=>{}
+// // //Arrow Function
+// // const myFunc =()=>{}
 
-// {
-// function Tampilkan(a){
-//     console.log("halo!"+ a);
-// }
-// Tampilkan("huda");
-// }
-// {
-// //Anonymous Function
-// function tampilkanNama(a){
-// console.log("halo,"+ a)
-// }
-// tampilkanNama("huda")
-// }
-// {
-// //Arrow Function
-// const tampilkanNama= (a) => {
-//     console.log("halo,"+ a)
-// }
-// }
+// // {
+// // function Tampilkan(a){
+// //     console.log("halo!"+ a);
+// // }
+// // Tampilkan("huda");
+// // }
+// // {
+// // //Anonymous Function
+// // function tampilkanNama(a){
+// // console.log("halo,"+ a)
+// // }
+// // tampilkanNama("huda")
+// // }
+// // {
+// // //Arrow Function
+// // const tampilkanNama= (a) => {
+// //     console.log("halo,"+ a)
+// // }
+// // }
 
-// //Latihan soal
-// // function lama
-// function HitungPersegi(sisi) {
+// // //Latihan soal
+// // // function lama
+// // function HitungPersegi(sisi) {
 
-// function luas (s) {return s * s}
-// function keliling (s) {return 4 * s}
-// console.log("luas:", luas (sisi))
-// console.log("keliling:",keliling(sisi))
-// }
-// HitungPersegi(6)
+// // function luas (s) {return s * s}
+// // function keliling (s) {return 4 * s}
+// // console.log("luas:", luas (sisi))
+// // console.log("keliling:",keliling(sisi))
+// // }
+// // HitungPersegi(6)
 
 
-// //anonymous
-// var HitungPersegi = function(sisi) {
-//     var luas = function(s) { return s * s; };
-//     var keliling = function(s) { return 4 * s; };
+// // //anonymous
+// // var HitungPersegi = function(sisi) {
+// //     var luas = function(s) { return s * s; };
+// //     var keliling = function(s) { return 4 * s; };
     
-//     console.log("luas:", luas(sisi));
-//     console.log("keliling:", keliling(sisi));
-// };
-// HitungPersegi(6);
+// //     console.log("luas:", luas(sisi));
+// //     console.log("keliling:", keliling(sisi));
+// // };
+// // HitungPersegi(6);
 
 
-// //Arrow Functions
-// var HitungPersegi=(sisi) => {
+// // //Arrow Functions
+// // var HitungPersegi=(sisi) => {
 
-//     var luas = (s) => s*s; 
-//     var keliling = (s) => 4*s;
+// //     var luas = (s) => s*s; 
+// //     var keliling = (s) => 4*s;
    
-//     console.log("luas:", luas(sisi));
-//     console.log("keliling:", keliling(sisi));
-// };
-// HitungPersegi(6);
+// //     console.log("luas:", luas(sisi));
+// //     console.log("keliling:", keliling(sisi));
+// // };
+// // HitungPersegi(6);
 
-// const Hitung_Persegi = (sisi) => {
-//     const luas = (s) => {
-//     return s * s; }
-//     const keliling = (s) => {
-//     return 4 * s;}
+// // const Hitung_Persegi = (sisi) => {
+// //     const luas = (s) => {
+// //     return s * s; }
+// //     const keliling = (s) => {
+// //     return 4 * s;}
     
-//     console.log("luas:", luas(sisi));
-//     console.log("keliling:", keliling(sisi));
-// };
+// //     console.log("luas:", luas(sisi));
+// //     console.log("keliling:", keliling(sisi));
+// // };
 
-// Hitung_Persegi(6);
+// // Hitung_Persegi(6);
 
 
-let nama = "Taufiqurrahman"
-console.log("halo, selamat datang."+nama)
-console.log("halo, selamat datang.   "+nama)
+// // let nama = "Taufiqurrahman"
+// // console.log("halo, selamat datang."+nama)
+// // console.log("halo, selamat datang.   "+nama)
 
-console.log("halo, "+nama +"! selamat datang")
-console.log(`halo, ${nama},selamat datang`)
+// // console.log("halo, "+nama +"! selamat datang")
+// // console.log(`halo, ${nama},selamat datang`)
 
-text1 = "hai"
-text2 = "namaku"
-text3 = "taufiq"
+// // text1 = "hai"
+// // text2 = "namaku"
+// // text3 = "taufiq"
 
-//Template Literal
-//template literal atau biasa di sebut template string merupakan fitur ES6 yang memungkinkan kita menyusun string dengan rapi dengan menggunakan tanda etik terbalik dan ${variabelnya}.
+// // //Template Literal
+// // //template literal atau biasa di sebut template string merupakan fitur ES6 yang memungkinkan kita menyusun string dengan rapi dengan menggunakan tanda etik terbalik dan ${variabelnya}.
 
-console.log(text1+", "+text2+" "+text3)
-console.log(`${text1}, ${text2}, ${text3}`)
+// // console.log(text1+", "+text2+" "+text3)
+// // console.log(`${text1}, ${text2}, ${text3}`)
+
+// //Enhanced object literals/
+// // Enhanced object literals merupakan fitur ES6 yang memungkinkan kita untuk kmenyederhanakan sebuah object,
+// // dimana biasanya kita selalu menulis property dan value,
+// // tetapi jika terdapat kondisi ada variabel yang namanya sama dengan property maka kita bisa assign hal tersebut sebagai value tetapi dengan hanya menulis property nya saja.
+
+// // let nama = "Taufiqurrahman"
+// // let person = {
+// //     nama : nama,    //Penggunaan object biasa
+// //     nama,           //Enhanced object literals
+// //     umur : 24,
+// // }
+// // console.log(person)
+
+// //Destructuring
+// //Destructuring merupakan ekspresi javascript yang memungkinkan untuk membagi atau memecah nilai dari sebuah array atau objek ke dalam variabel yang berbeda.
+
+// //Destructuring Array
+
+// // let numb = [1,3,5,7]    //bentuk biasa
+
+// // let numb1= numb[0]      //bentuk biasa
+// // let numb2= numb[1]      //bentuk biasa
+// // let numb3= numb[2]      //bentuk biasa
+// // let numb4= numb[3]      //bentuk biasa
+// // let numb5= numb[4]      //bentuk biasa
+
+// // console.log(numb4)      //bentuk biasa
+
+// let [num1,num2,num3,num4,lastnum] = [1,5,6,7,8] //Destructuring Array
+
+// console.log(num1)
+
+// //bentuk object biasa
+// //Destructuring Object
+// let name = "Taufiqurrahman"
+// let person = {
+//     // name   : name,    //Penggunaan object biasa
+//     name,                //penulisan Enhanced object literals
+//     tinggi : 160,
+//     umur   : 24
+// }
+
+// let namaPerson = person.nama        //Enhanced object literals
+// let umurPerson = person.umur        //Enhanced object literals
+// let tinggiPerson = person.tinggi    //Enhanced object literals
+// let {nama,umur,tinggi} = person     //Enhanced object literals
+
+// console.log(person)
+
+//Rest Parameter + Spread Operator
+//Rest Parameters dan Spread Operator di lambangkan dengan simbol yang sama yaitu "..."
+
+//Rest Parameter
+//Rest Parameter ini berguna untuk menggabungkan semua paramater pada function ke dalam array. 
+//Dengan menggunakan Rest Parameter ini dapat membantu kita mendefinisikan function dengan rapi serta memberikan parameter yang tidak terbatas pada sebuah function.
+
+let [num1, , , , lastnum] = [1,5,6,7,8]
+
+let footballers = ["Messi", "Ronaldo", "Neymar","Halland", "Mbappe"]
+let [foot1, foot2, ...restFoots] = footballers
+
+console.log(foot1)
+console.log(foot2)
+console.log(restFoots)      //rest parameter
+console.log(restFoots[1])   //select rest parameter
+
