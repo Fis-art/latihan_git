9ed15a6bcaf8d564882cea711b316ce380f5f668
taufiq <taufiq6040@gmail.com>
2026-06-30 15:05:17 +0800

    index

--- my_express_app/index.js
+++ my_express_app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('saya akan kembali ke kota saya!');
+  res.send('halo dunia!');
 });
 
 app.listen(port, () => {
