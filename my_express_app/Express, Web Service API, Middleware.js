a45b9b2cd11e2ae955b055de22d8fbabe2d5cfda
taufiq <taufiq6040@gmail.com>
2026-07-01 13:39:28 +0800

    Pengenalan Express.js, Web Service API & Middleware

---
+++ my_express_app/Express, Web Service API, Middleware.js
@@ -0,0 +1,32 @@
+//Pengenalan Express.js,
+//Web Service API &
+//Middleware
+
+
+
+//Apa itu Express.js
+
+//Express.js adalah framework web app untuk Node.js yang ditulis dengan bahasa pemrograman JavaScript. 
+// Framework open source ini dibuat oleh TJ Holowaychuk pada tahun 2010 lalu. 
+// Express.js adalah framework back end. 
+// Artinya, ia bertanggung jawab untuk mengatur fungsionalitas website, seperti pengelolaan routing dan session, permintaan HTTP, penanganan error, serta pertukaran data di server. 
+// Framework yang satu ini punya arsitektur MVC (Model View Controller). Dengan begitu, setiap data diolah pada Model, dihubungkan melalui Controller, lalu ditampilkan menjadi informasi melalui View.
+
+// Kelebihan dan Kekurangan Express.js
+// Kelebihan 
+// Bebas menentukan sendiri arsitektur dan struktur website yang akan dikembangkan
+
+// Kekurangan
+// Diperlukan penelusuran kode lebih untuk membuat, mengelola, dan merawat arsitektur website yang sudah dibuat sebelumnya dengan express
+
+// Kelebihan
+// Ukuran framework lebih kecil dan ringan, karena hanya berisi package inti
+
+// Kekurangan
+// Terlalu banyak pilihan plugin dan library untuk digunakan, bisa membingungkan bagi sebagian orang
+
+// Kelebihan
+// Kinerja website yang dihasilkan jadi lebih baik, mengingat ringannya framework
+
+// Kekurangan
+// Pilihan metode pengembangan yang berbeda-beda, sehingga tidak ada standarbaku
