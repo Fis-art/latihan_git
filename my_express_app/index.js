de05db001f158dfeed44e30539b3d5b7ff368ace
taufiq <taufiq6040@gmail.com>
2026-07-01 10:53:57 +0800

    index

--- my_express_app/index.js
+++ my_express_app/index.js
@@ -2,9 +2,62 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let mahasiswa = ['adit', 'debby', 'heldi', 'taufik', 'andra', 'desta']
+let objectMahasiswa = [
+  {
+    nim : 223344,
+    nama : "Aditya Saputra",
+  },
+  {
+    nim : 235671,
+    nama : "Debby Afrizal",
+  },
+  {
+    nim : 233121,
+    nama : "Heldi Saputra"
+  }
+]
+
+const getobjectMahasiswa = (req, res) => {
+  let result = ""
+  objectMahasiswa.forEach((item, index) => {
+    result += `<h1>${index +1}. Nama :${item.nama} NIM: ${item.nim}</h1>`
+  })
+  res.send("berikut data mahasiswa: " + result);
+}
+
+const getMahasiswa = (req, res) => {
+  let result = ""
+  mahasiswa.forEach(function(item, index){
+    result += `<h1> ${index +1}. ${item}</h1>`
+  })
+  res.send("berikut daftar mahasiswa: " + result);
+};
+
+const getMahasiswaByNim = (req, res) => {
+  const nim = req.params.nim;
+  const mahasiswa = objectMahasiswa.find((item) => {
+    return item.nim === Number(nim);
+  });
+if (mahasiswa) {
+
+    res.send(`<h1>Nama: ${mahasiswa.nama}, NIM: ${mahasiswa.nim}</h1>`);
+
+  } else {
+    res.status(404).send('<h1>Mahasiswa tidak ditemukan</h1>');
+  }
+};
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('halo dunia!');
+  res.send('HALO SELAMAT DATANG BROO');
 });
+
+app.get('/objectmahasiswa/:nim', getMahasiswaByNim);
+app.get('/mahasiswa', getMahasiswa);
+app.get('/objectmahasiswa', getobjectMahasiswa)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
