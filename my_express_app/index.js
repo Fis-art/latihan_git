bba9c86dd15a1493629efa0fc2fa73c613935ce1
taufiq <taufiq6040@gmail.com>
2026-07-03 14:05:57 +0800

    latihan mandiri

--- my_express_app/index.js
+++ my_express_app/index.js
@@ -213,20 +213,23 @@ app.use(loggerMiddleware);
 
 app.get('/movies', getMovie);
 
+//route dimatikan karena pencarian year sudah digabung ke getMovie
 // app.get('/movies/year', getMovieByYear);
 
 app.get('/movies/:id', getMovieById);
 
+//tokenMiddleware dimatikan
 app.get('/api/movies',
     // tokenMiddleware,
     getMovieApi
 );
 
-app.get('/api/movies/:id', // Pasang di /api/movies/:id
+app.get('/api/movies/:id', // tokenMiddleware Pasang di /api/movies/:id
     tokenMiddleware,
     getMovieByIdApi
 );
 
+//route dimatikan karena pencarian year sudah digabung ke getMovieApi
 // app.get('/api/movies/year',
 //     tokenMiddleware,
 //     getMovieByYearApi
