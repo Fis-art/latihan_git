91424cace5715737ca5fc0ad97dd47043e204a79
taufiq <taufiq6040@gmail.com>
2026-07-03 12:01:40 +0800

    loggerMiddleware

--- my_express_app/index.js
+++ my_express_app/index.js
@@ -199,6 +199,13 @@ app.listen(port, () => {
 //http://localhost:3000/api/movies?token=12345
 //search by id
 //http://localhost:3000/api/movies/2
+//search by title
+//http://localhost:3000/api/movies?title=John
+//search by year
+//http://localhost:3000/api/movies/year?year=2014
+//search by title and year
+//http://localhost:3000/api/movies?token=12345&title=John&year=2014
+//with token
 //http://localhost:3000/api/movies/2?token=12345
 //search by title with token
 //http://localhost:3000/api/movies?token=12345&title=Spider
@@ -206,3 +213,5 @@ app.listen(port, () => {
 //http://localhost:3000/api/movies/year?token=12345&year=2020
 //search by year with token
 //http://localhost:3000/api/movies/year?token=12345&year=2014
+//search by title and year with token
+//http://localhost:3000/api/movies?token=12345&title=John&year=2014
