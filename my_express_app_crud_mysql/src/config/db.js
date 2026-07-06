125ab88791db886891a77786a3789dd9a76da845
taufiq <taufiq6040@gmail.com>
2026-07-06 15:17:46 +0800

    env

--- my_express_app_crud_mysql/src/config/db.js
+++ my_express_app_crud_mysql/src/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config()
 
 const connectionPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "db_movies",
-    port : 3306
+    host: process.env.DB.HOST,
+    user: process.env.DB.USER,
+    password: process.env.DB.PASSWORD,
+    database: process.env.DB.DATABASE,
+    port : process.env.DB.PORT,
 });
 
 connectionPool.getConnection(function(err) {
