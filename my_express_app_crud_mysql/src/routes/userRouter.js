9c16e2e4cac17ad80a4d58c1e971e7557f686ebb
taufiq <taufiq6040@gmail.com>
2026-07-06 16:31:29 +0800

    buat user register

---
+++ my_express_app_crud_mysql/src/routes/userRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const userRouter = express.Router();
+
+userRouter.get('/login', login)
+userRouter.get('/register', register)
+
+module.exports = {
+    userRouter
+
+}
